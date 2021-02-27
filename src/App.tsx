0a047b20833d2d2c49c65aaed294f0aa0a991197
rui-watanabe <wataru610806@gmail.com>
2021-02-27 15:42:45 +0900

    create card component #5

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import Counter from './features/counter/Counter';
 import './App.css';
+import Cards from './features/covid/Cards/Cards';
 
 const App: React.FC = () => {
   return (
@@ -51,6 +52,7 @@ const App: React.FC = () => {
           </a>
         </span>
       </header>
+      <Cards />
     </div>
   );
 };
